7f0a14bc3aa902aba8548f7e6fd314817760dc3a
Isa cabrera.S <158528449+IsabellaCabrera@users.noreply.github.com>
2024-08-16 17:43:57 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//EJ 1
+const tasks = [
+    { id: 1 , name: "comprar mercado" , completada: true },
+    { id: 2 , name: "hacer ejercicio" , completada: false },
+    { id: 3 , name: "limpiar casa" , completada: false },
+    { id: 4 , name: "hacer tareas" , completada: false },
+]
+
+const taskStatus = (tasks, id) => {
+    tasks.forEach(task => { 
+    if ( tasksCompletada === task.id){
+       task.id = true
+    
+    } 
+
+    });
+};
+console.log(taskStatus(task,2))
+
+ //EJ 2
+ const cities =
+ [
+    { name: "cali", population: 111.000 },
+    { name: "bogota", population: 333.000 },
+    { name: "cali", population: 5000.0000 }
+ ]
+
+ const maxPopulation = (cities) => {
+    let arry = [0]
+   cities.forEach(city => {
+    if ( city.population )
+   });
+ }
